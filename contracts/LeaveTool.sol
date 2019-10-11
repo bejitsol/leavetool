fbe0b37329400bb56698a78b343c00f1b2f290fe
Bevan Joyce <bjoycedev@gmail.com>
2019-10-11 12:40:14 +0200

    Added set all leave

--- contracts/LeaveTool.sol
+++ contracts/LeaveTool.sol
@@ -1,13 +1,17 @@
-pragma solidity 0.5.8;
+pragma solidity ^0.5.8;
 
 contract LeaveTool {
-	// Employee model
 
+	address public owner;
+
+	uint8 power;
+	// Employee model
 	struct Employee {
 		uint id;
 		string username;
 		string fullname;
 		string location;
+		uint16 leaveTotal;
 	}
 
 	// Read/Write Employees
@@ -16,12 +20,30 @@ contract LeaveTool {
 	//Employee count
 	uint public employeeCount;
 
+
+	modifier onlyOwner {
+		require(msg.sender == owner, "Unauthorized");
+		_;
+	}
+
 	constructor () public {
+		owner = msg.sender;
+		intCalculator = 10;
 		addEmployee("joycbeva", "Bevan Joyce", "CPT");
+	}
+
+
+	//Can only be performed by the owner of the contract
+	function setAllEmployeeLeave(uint8 _leaveAmount) public onlyOwner {
+		require(employeeCount > 0);
+
+		for(uint8 i=1; i <= employeeCount; i++){
+			employees[i].leaveTotal = _leaveAmount;
+		}
 	}
 
 	function addEmployee (string memory _username, string memory _fullname, string memory _location) private {
 		employeeCount++;
-		employees[employeeCount] = Employee(employeeCount, _username, _fullname, _location);
+		employees[employeeCount] = Employee(employeeCount, _username, _fullname, _location, 0);
 	}
 }
